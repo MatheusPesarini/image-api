caa5a576c0cd9339c585efbbd68b4288e72eaf0c
Matheus <mrogeriopesarini@gmail.com>
2025-04-07 23:25:25 -0300

    feat: initialize image API with Express server and package.json
    - Created package.json to manage project dependencies and metadata.
    - Added Express as a dependency for handling HTTP requests.
    - Implemented a basic Express server in server.js to serve static images.
    - Configured server to listen on port 4000 and serve images from the 'images' directory.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('node:path');
+
+const app = express();
+const port = 4000;
+
+app.use(express.static(path.join(__dirname, 'images')));
+
+app.get('/', (req, res) => {
+  res.send('Servidor Express de imagens local está funcionando!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Imagens estão disponíveis a partir de http://localhost:${port}/images/`);
+  console.log(`Exemplo: http://localhost:${port}/images/camisetas/nome_da_imagem.jpg`);
+});
